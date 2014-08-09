2d3c2bba12260b61eb72155208b9727591d58dc0
Mitchell Perilstein <mitchell.perilstein@gmail.com>
2014-08-09 01:16:14 -0400

    crude first cut working. needs a ring buffer.

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ Yes, it does, but it only shows connections in progress.  Transients, for exampl
 
 It just ties into the connect(2) system call, writing info such as the caller's pid, destination address and family to readers of /dev/connwatch.  If there is a better way to do this, I'm all ears.
 
+**How about SystemTap?**
+
+To be investigated.  It looks heavy weight and general.
+
 **Then what are all the scripty bits for?**
 
 They keep track of rules and reporting. White lists are places you frequent and expect your applications to be contacting.  Some domain specific configuration language will tie regex on domains or IP to actions like ignore, log, report, etc.  TBD.
--- connwatch.c
+++ connwatch.c
@@ -1,5 +1,9 @@
 /*
+ * Kernel module to log connections per-PID
+ * Various snippets borrowed from Linux chardev.c and kprobe_example.c
  *
+ * Copyright (C) 2014 Mitchell Perilstein
+ * Licensed under GNU GPL Version 2. See LICENSING file for details.
  */
 
 #include <linux/kernel.h>
@@ -10,23 +14,39 @@
 #include <linux/socket.h>
 #include <linux/in.h>
 #include <linux/ip.h>
+#include <linux/cdev.h>
 #include <asm/byteorder.h>
 
-#define MYNAME   "connwatch"
-#define FILENAME "/dev/connwatch"
+#define DEVICE_NAME 	"connwatch"
+#define FILENAME    	"/dev/connwatch"
+#define SUCCESS		0
+#define BUF_LEN		1024
 
-struct event {
-    
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+
+static struct file_operations fops = {
+    .read = device_read,
+    .open = device_open,
+    .release = device_release
 };
 
 const char *dgram  = "inet_dgram_connect";
 const char *stream = "inet_stream_connect";
 
+static dev_t dev;
+static struct cdev *my_cdev;
+
+static int Device_Open = 0;	/* Is device open? Used to prevent multiple access to device */
+static char msg[BUF_LEN];	/* The msg the device will give when asked */
+static char *msg_Ptr;
+
 static char *my_inet_ntoa(struct sockaddr_in *addr)
 {
     unsigned char *bytes = (unsigned char *)&(addr->sin_addr.s_addr);
     static char buffer[18];
-    snprintf(buffer, sizeof(buffer), "%d.%d.%d.%d", 
+    snprintf(buffer, sizeof(buffer), "%d.%d.%d.%d",
              bytes[0], bytes[1], bytes[2], bytes[3]);
     return buffer;
 }
@@ -39,9 +59,13 @@ static void report(char *type, struct sockaddr_in *uaddr)
         ! ipv4_is_private_192(uaddr->sin_addr.s_addr) &&
         ! ipv4_is_zeronet(uaddr->sin_addr.s_addr)     &&
         ! ipv4_is_loopback(uaddr->sin_addr.s_addr)) {
-        printk("%s %s port:%d addr:%s from %s pid %d\n", 
-               MYNAME, type, ntohs(uaddr->sin_port), 
-               my_inet_ntoa(uaddr), current->comm, current->pid);
+
+        /* TODO - make this a ring buffer to avoid overwriting messages
+           that haven't been read by consumer yet. */
+
+        sprintf(msg, "%s port:%d addr:%s from %s pid %d\n",
+               type, ntohs(uaddr->sin_port), my_inet_ntoa(uaddr), current->comm, current->pid);
+        msg_Ptr = msg;
     }
 }
 
@@ -64,61 +88,107 @@ static int my_dgram_connect(void *sock, struct sockaddr_in *uaddr, size_t addrle
 }
 
 static struct jprobe my_stream_jprobe = {
-        .entry = (kprobe_opcode_t *) my_stream_connect
+    .entry = (kprobe_opcode_t *) my_stream_connect
 };
 
 static struct jprobe my_dgram_jprobe = {
-        .entry = (kprobe_opcode_t *) my_dgram_connect
+    .entry = (kprobe_opcode_t *) my_dgram_connect
 };
 
-int plant(struct jprobe *probe, const char *thing)
+static int plant(struct jprobe *probe, const char *thing)
 {
     int ret;
 
     probe->kp.addr = (kprobe_opcode_t *) kallsyms_lookup_name(thing);
     if (!probe->kp.addr) {
-        printk("%s Couldn't find %s to plant jprobe\n", MYNAME, thing);
+        printk("%s Couldn't find %s to plant jprobe\n", DEVICE_NAME, thing);
         return -1;
     }
 
     if ((ret = register_jprobe(probe)) < 0) {
-        printk("%s register_jprobe %s failed, returned %d\n", MYNAME, thing, ret);
+        printk("%s register_jprobe %s failed, returned %d\n", DEVICE_NAME, thing, ret);
         return -1;
     }
 
-    printk("%s planted %s jprobe at %p, handler addr %p\n", MYNAME, thing,
+    printk("%s planted %s jprobe at %p, handler addr %p\n", DEVICE_NAME, thing,
            probe->kp.addr, probe->entry);
 
-    return 0;
+    return SUCCESS;
 }
 
-int init_module(void)
+static int device_open(struct inode *sip, struct file *sfp)
 {
-        int ret;
-        dev_t dev;
-        
-        ret = alloc_chrdev_region(&dev, 0, 1, MYNAME);
-        if (ret < 0) return ret;
+    if (Device_Open)
+        return -EBUSY;
 
-        printk("%s allocated %d, %d\n", MYNAME, MAJOR(dev), MINOR(dev));
+    Device_Open++;
+    //    sprintf(msg, "ready\n");
+    // msg_Ptr = msg;
+    try_module_get(THIS_MODULE);
+    
+    return SUCCESS;
+}
 
-        ret = plant(&my_stream_jprobe, stream);
-        if (ret < 0) return ret;
+static ssize_t device_read(struct file *ignore, char *buffer, size_t length, loff_t *ignore2)
+{
+    int copied = 0;
 
-        ret = plant(&my_dgram_jprobe, dgram);
-        if (ret < 0) return ret;
-
+    if (*msg_Ptr == 0)
         return 0;
+
+    while (length && *msg_Ptr) {
+        put_user(*(msg_Ptr++), buffer++);
+        length--;
+        copied++;
+    }
+
+    return copied;
 }
 
-void cleanup_module(void)
+static int device_release(struct inode *sip, struct file *sfp)
 {
+    Device_Open--;		/* We're now ready for our next caller */
+    module_put(THIS_MODULE);
+
+    return SUCCESS;
+}
+
+static int __init connwatch_init(void)
+{
+    int ret;
+
+    ret = alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME);
+    if (ret < 0) return ret;
+
+    my_cdev = cdev_alloc();
+    my_cdev->owner = THIS_MODULE;
+    my_cdev->ops = &fops;
+    ret = cdev_add(my_cdev, dev, 1);
+    if (ret < 0) return ret;
+
+    printk("%s allocated %d, %d\n", DEVICE_NAME, MAJOR(dev), MINOR(dev));
+
+    ret = plant(&my_stream_jprobe, stream);
+    if (ret < 0) return ret;
+
+    ret = plant(&my_dgram_jprobe, dgram);
+    if (ret < 0) return ret;
+
+    return SUCCESS;
+}
+
+static void __exit_call connwatch_cleanup(void)
+{
+    cdev_del(my_cdev);
     unregister_chrdev_region(dev, 1);
     unregister_jprobe(&my_stream_jprobe);
     unregister_jprobe(&my_dgram_jprobe);
-    printk("%s unregistered\n", MYNAME);
+    printk("%s unregistered\n", DEVICE_NAME);
 }
 
+module_init(connwatch_init)
+module_exit(connwatch_cleanup)
+
 MODULE_AUTHOR("Mitchell Perilstein");
-MODULE_DESCRIPTION("connwatch - Report new network connections");
+MODULE_DESCRIPTION("connwatch - Report new network connections by PID as they occur");
 MODULE_LICENSE("GPL v2");
